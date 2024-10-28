26540de86480c0a656ee3020f12af3e2599b3d18
Arun <arunkumar.zeoner@gmail.com>
2024-10-28 10:33:12 +0530

    initial2

--- my-vite-app/src/components/Authorize.tsx
+++ my-vite-app/src/components/Authorize.tsx
@@ -5,7 +5,7 @@ import FHIR from 'fhirclient';
 const Authorize: React.FC = () => {
   useEffect(() => {
     FHIR.oauth2.authorize({
-      client_id: '<your-client-id>',
+      client_id: '7f70d5fd-b715-4ac6-b1db-1d5cc3182f2e',
       scope: 'patient/Patient.read patient/Observation.read launch online_access openid profile',
       redirectUri: 'https://movin-smart.github.io/react-fhir/my-vite-app/callback'
     });
